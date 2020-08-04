2b5887a72d858fef8dca14e578bc9e80e74360d5
leijiaxin <493247471@qq.com>
2020-08-04 10:36:12 +0800

    first commit

--- src/components/fileViewRegister.js
+++ src/components/fileViewRegister.js
@@ -31,6 +31,14 @@ function getName(url) {
     return fileName.substr(0, fileName.lastIndexOf('.'));
 }
 
+/**
+ *
+ * @param fileList 文件数组
+ * @param viewIndex 默认第一个展示文件的index
+ * @param downLoad 是否有下载按钮 有的话参数为function 回调参数为当前展示文件对象数据
+ * @param dKey 列表展示文件循环绑定的key
+ * @returns {{downLoad: *, dKey: string, viewIndex: number, fileList: *}}
+ */
 export default (fileList = [], viewIndex = 0, downLoad = null, dKey = 'url') => {
     const showFileDom = new fileViewConstructor({
         el: document.createElement('div'),
